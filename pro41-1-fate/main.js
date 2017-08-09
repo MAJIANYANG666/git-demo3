554a50952b607af66a8aec684f380d036c8b09d1
MAJIANYANG666 <1297606787@qq.com>
2017-08-09 23:07:12 +0800

    添加懒加载和预加载

--- pro41-1-fate/main.js
+++ pro41-1-fate/main.js
@@ -1,48 +1,77 @@
-let n=1
-let loading=false;
-let hasNext=true
-function YYY(){
-    if(loading===true){return}
-    let request=new XMLHttpRequest()
-    request.open('GET',`./page-${n+1}.html`)  //同级目录的...
-    request.onerror=function(){
-        loading=false;
+// var loadingImage=new Image()
+// loadingImage.src="https://i.loli.net/2017/08/09/598ad38cb91b2.gif"
+let n = 1
+let loading = false;
+let hasNext = true;
+function YYY() {
+    if (loading === true) {
+        return
     }
-    request.onload=function(){
-        loading=false;
-        n+=1
-        let response=request.responseText
-        let data =window.JSON.parse(response)
-        for(let i=0;i<data.content.length;i++){
-            let liString=`
+    let request = new XMLHttpRequest()
+    request.open('GET', `./page-${n + 1}.html`)  //同级目录的...
+    request.onerror = function () {
+        loading = false;
+    }
+    request.onload = function () {
+        loading = false;
+        n += 1
+        let response = request.responseText
+        let data = window.JSON.parse(response)
+        for (let i = 0; i < data.content.length; i++) {
+            let liString = `
                 <li>
-                  <div class="oneToOne"><img src="${data.content[i].url}" ></div>
+                  <div class="oneToOne"><img src="https://i.loli.net/2017/08/09/598ad38cb91b2.gif" data-xxx="${data.content[i].url} " ></div>
                   <h3>${data.content[i].text}</h3>
                   <p>${data.content[i].p}</p>
                 </li>
                `
-            list.insertAdjacentHTML('beforeend',liString);
+            list.insertAdjacentHTML('beforeend', liString);
         }
-        if(data.hasNextPage===false){
-            hasNext=false;
-            loadMoreButton.textContent='没了'
+        if (data.hasNextPage === false) {
+            hasNext = false;
+            loadMoreButton.textContent = '没了'
         }
     }
-    loading=true;
+    loading = true;
     request.send()
 }
-loadMoreButton.onclick=YYY
-window.onscroll=function(){
-    var doc = document.documentElement;
-    //var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-    var viewportOffset = loadMoreButton.getBoundingClientRect();
-    // these are relative to the viewport, i.e. the window
-    var buttonTop = viewportOffset.top;
-    var clientHeight=doc.clientHeight;
-    if(buttonTop>clientHeight-100){
-    } else{
-        if(hasNext===true){loadMoreButton.click()}
+loadMoreButton.onclick = YYY
+window.onscroll = function () {
+    if (f1(loadMoreButton)) {
+        if (hasNext === true) {
+            loadMoreButton.click()
+        }
     }
-}/**
+    let images=document.querySelectorAll('img[data-xxx]')
+    console.log(images)
+    for(let i=0;i<=images.length;i++){
+        console.log(f1(images[i]))
+        console.log(images[i])
+        if(f1(images[i])){
+
+            images[i].src=images[i].getAttribute('data-xxx')
+            images[i].removeAttribute('data-xxx')
+        }
+    }
+
+
+    function f1(element){
+        var doc = document.documentElement;
+        var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+
+        var viewportOffset = element.getBoundingClientRect();
+
+        // these are relative to the viewport, i.e. the window
+        var buttonTop = viewportOffset.top;
+        var clientHeight = doc.clientHeight;
+        if (buttonTop <= clientHeight ) {
+            return true
+        }else{
+            return false
+        }
+    }
+}
+
+/**
  * Created by Administrator on 2017/8/8.
  */
